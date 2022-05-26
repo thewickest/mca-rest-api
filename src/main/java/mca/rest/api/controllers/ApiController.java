4475e3e4279f46a1d07684f62dc09c9db0033eec
The Wickest <graubuntu@gmail.com>
2022-05-26 15:43:25 +0200

    Initial commit

---
+++ src/main/java/mca/rest/api/controllers/ApiController.java
@@ -0,0 +1,38 @@
+package mca.rest.api.controllers;
+
+import org.json.simple.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ *
+ * A middleWare Api that returns Similar objects to the given one by Id.
+ */
+@RestController
+public class ApiController {
+    /**
+     * Retrieves an array of similar objects of the given one by
+     *
+     * @param The Id of the product
+     * @return Similar Objects to the Object given by Id
+     */
+    @SuppressWarnings("unchecked")
+	@RequestMapping(value = "/product/{productId}/similar", 
+    				method = RequestMethod.GET,
+    				produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public JSONArray getSimilar(@PathVariable String productId) {
+    	int[] array = MockController.getSimilarIds(productId);
+    	JSONArray res = new JSONArray();
+        for(int i=0; i<array.length; i++) {
+        	res.add(MockController.getProduct(array[i]));
+        }
+        return res;
+    }
+}
---
+++ src/main/java/mca/rest/api/controllers/MockController.java
@@ -0,0 +1,50 @@
+package mca.rest.api.controllers;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.client.RestTemplate;
+
+public class MockController {
+	
+	private final static String uriMock = "http://localhost:3001/";
+	private final static String similarIds = "product/%s/similarids";
+	private final static String product = "product/%s";
+	
+	/**
+	 * Method that return the ids of similiar product of the given one
+	 * 
+	 * @param productId The Id of the product
+	 * @return An array with the Ids of similiar products
+	 */
+	public static int[] getSimilarIds(String productId) {
+		RestTemplate restTemplate = new RestTemplate();
+		StringBuilder uri = new StringBuilder(uriMock);
+		uri.append(String.format(similarIds, productId));
+		return restTemplate.getForObject(uri.toString(), int[].class);
+	}
+	
+	/**
+	 * Retrieves a product by Id
+	 * 
+	 * @param productId The Id of the product
+	 * @return A JsonObject of the product
+	 */
+	public static JSONObject getProduct(int productId) {
+		JSONParser parser = new JSONParser();
+		
+		RestTemplate restTemplate = new RestTemplate();
+		StringBuilder uri = new StringBuilder(uriMock);
+		uri.append(String.format(product, productId));
+		String res =  restTemplate.getForObject(uri.toString(), String.class);
+		JSONObject json = null;
+		try {
+			json = (JSONObject) parser.parse(res);
+			System.out.println(json);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
+
+}
